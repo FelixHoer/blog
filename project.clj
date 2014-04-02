b5270f76e9d4877b4320fff31edef57ad8b46597
FelixHoer <felix.hoerandner@gmx.at>
2014-04-02 22:42:10 +0200

    added data retrieval

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
                  [ring/ring-core "1.2.2"]
                  [ring/ring-jetty-adapter "1.2.2"]
                  [compojure "1.1.6"]
-                 [de.ubercode.clostache/clostache "1.3.1"]]
+                 [de.ubercode.clostache/clostache "1.3.1"]
+                 [markdown-clj "0.9.41"]]
   :uberjar-name "blog-standalone.jar")
--- src/blog/core.clj
+++ src/blog/core.clj
@@ -5,12 +5,20 @@
             [ring.middleware.session :as session]
             [ring.middleware.params :as params]
             [ring.util.response :as ring-response]
-            [clostache.parser :as clostache]))
+            [clostache.parser :as clostache]
+            [markdown.core :as markdown]))
+
 
 ; constants
 
 (def STATIC_RESOURCE_PATH "static")
 (def TEMPLATES_RESOURCE_PATH "templates")
+(def ARTICLES_PATH "articles")
+
+(def ARTICLES_PER_PAGE 5)
+(def RECENT_ARTICLES 10)
+(def DATE_PREFIX_LENGTH 7)
+
 
 ; helpers
 
@@ -18,6 +26,7 @@
   (ring-response/redirect (str "http://" server (if port (str ":" port)) path)))
 
 (str nil)
+
 
 ; templates
 
@@ -42,6 +51,49 @@
 
 (def article-partial (templates "article"))
 
+
+; logic
+
+(defn article-files []
+  (let [root (clojure.java.io/file ARTICLES_PATH)
+        files (filter #(.isFile %) (file-seq root))]
+    files))
+
+(defn sort-by-name [files]
+  (sort-by #(.getName %) files))
+
+(defn group-by-month [files]
+  (let [date-prefix-f #(clojure.string/join (take DATE_PREFIX_LENGTH (.getName %)))
+        grouped-files (group-by date-prefix-f files)]
+    grouped-files))
+
+(defn article-data [name]
+  (let [safe-name (clojure.string/join (remove #(= \/ %) name))
+        path (str ARTICLES_PATH "/" safe-name)
+        file-content (slurp path)
+        html (markdown/md-to-html-string file-content)]
+    html))
+
+(defn article-page-data [page-num article-files]
+  (let [sorted-files (sort-by-name article-files)
+        pages (partition-all ARTICLES_PER_PAGE sorted-files)
+        page (nth pages page-num)]
+    (map article-data page)))
+
+(defn article-month-data [month article-files]
+  (let [month-files (get (group-by-month article-files) month)
+        file-names (map #(.getName %) month-files)]
+    (map article-data file-names)))
+
+(defn recent-article-data [article-files]
+  (let [sorted-files (sort-by-name article-files)
+        recent-titles (map #(.getName %) (take RECENT_ARTICLES sorted-files))]
+    recent-titles))
+
+(defn archive-article-data [article-files]
+  (keys (group-by-month article-files)))
+
+
 ; server endpoints
 
 (defn login [req]
@@ -63,6 +115,7 @@
 (defn show-article [id session]
   {:body (article-template {:id id})})
 
+
 ; authentication
 
 (defn is-logged-in? [session]
@@ -72,6 +125,7 @@
   `(if-not (is-logged-in? (:session ~req))
     (local-redirect ~req "/login")
     ~form))
+
 
 ; routes
 
@@ -87,6 +141,7 @@
   (route/resources "/" {:root STATIC_RESOURCE_PATH})
   (route/not-found
     "Page not found"))
+
 
 ; ring setup
 
