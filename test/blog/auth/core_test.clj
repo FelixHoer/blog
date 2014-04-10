bd22119ba7023761028f03c6759ff254bb3fa2a9
FelixHoer <felix.hoerandner@gmx.at>
2014-04-10 20:56:58 +0200

    updated tests

--- test/blog/auth/core_test.clj
+++ test/blog/auth/core_test.clj
@@ -1,18 +1,8 @@
-(ns blog.core-test
+(ns blog.auth.core-test
   (:require [clojure.test :refer :all]
-            [blog.core :refer :all]))
+            [blog.auth.core :refer :all]))
 
-(def test-articles
-  ["2014-04-02-first-post.md"
-   "2014-05-01-next-post.md"])
-
-(deftest data
-  (testing "group-by-month"
-    (is (= (group-by-month test-articles)
-           {"2014-05" ["2014-05-01-next-post.md"],
-            "2014-04" ["2014-04-02-first-post.md"]}))))
-
-(deftest helpers
+(deftest auth-helpers
   (testing "local-redirect"
     (is (= (local-redirect {:server-name "localhost"
                             :server-port "8080"}
@@ -36,13 +26,4 @@
                            "/next")
            {:status 302,
             :headers {"Location" "http://localhost/next"},
-            :body ""})))
-  (testing "parse-article-code"
-    (is (let [res (parse-article-code "2014-02")]
-           (and (= (:month-name res) "February")
-                (= (:year res) "2014"))))
-    (is (= (parse-article-code "2014-04-02-first-post")
-           {:code "2014-04-02-first-post",
-            :title "First Post",
-            :month-name "April",
-            :day "02", :month "04", :year "2014"}))))
+            :body ""}))))
---
+++ test/blog/content/helpers_test.clj
@@ -0,0 +1,50 @@
+(ns blog.content.helpers-test
+  (:require [clojure.test :refer :all]
+            [blog.content.helpers :refer :all]))
+
+(deftest helpers
+  (testing "month-name"
+    (is (= (month-name "02")
+           "February")))
+  (testing "code->filename"
+    (is (= (code->filename "some-article")
+           "some-article.md")))
+  (testing "filename->code"
+    (is (= (filename->code "some-article.md")
+           "some-article")))
+  (testing "parse-article-code"
+    (is (let [res (parse-article-code "2014-02")]
+           (and (= (:month-name res) "February")
+                (= (:year res) "2014"))))
+    (is (= (parse-article-code "2014-04-02-first-post")
+           {:code "2014-04-02-first-post",
+            :title "First Post",
+            :month-name "April",
+            :day "02", :month "04", :year "2014"})))
+  (testing "parse-article-filename"
+    (is (= (parse-article-filename "2014-04-02-first-post.md")
+           {:code "2014-04-02-first-post",
+            :title "First Post",
+            :month-name "April",
+            :day "02", :month "04", :year "2014"}))))
+
+(deftest pagination
+  (testing "paginate"
+    (is (= (paginate 0 3 [1 2 3 4 5 6 7 8])
+           {:next-page 1
+            :current-page 0
+            :items '(1 2 3)}))
+    (is (= (paginate 1 3 [1 2 3 4 5 6 7 8])
+           {:next-page 2
+            :previous-page 0
+            :current-page 1
+            :items '(4 5 6)}))
+    (is (= (paginate 2 3 [1 2 3 4 5 6 7 8])
+           {:previous-page 1
+            :current-page 2
+            :items '(7 8)})))
+  (testing "pagination-urls"
+    (is (= (pagination-urls #(str "/some/url/page/" %)
+                            {:next-page 10 :previous-page 8 :items '(1 2 3)})
+           {:next-page "/some/url/page/10"
+            :previous-page "/some/url/page/8"}))))
