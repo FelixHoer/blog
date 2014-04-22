ed4af1bad24208b69ca36ee1b2a2a519b9a40266
FelixHoer <felix.hoerandner@gmx.at>
2014-04-22 19:36:47 +0200

    added tests for the article handler

--- test/blog/article/article_file_datastore_impl_test.clj
+++ test/blog/article/article_file_datastore_impl_test.clj
@@ -2,24 +2,45 @@
   (:require [clojure.test :refer :all]
             [blog.article.article-file-datastore-impl :refer :all]))
 
-(def test-articles
-  ["2014-04-02-first-post.md"
-   "2014-05-01-next-post.md"])
+(def test-article-names
+  ["2014-05-01-next-post.md"
+   "2014-04-15-april-post.md"
+   "2014-04-02-first-post.md"])
 
 (deftest helpers
   (testing "code->filename"
     (is (= (code->filename "some-article")
            "some-article.md")))
+
   (testing "filename->code"
     (is (= (filename->code "some-article.md")
            "some-article")))
+
   (testing "parse-article-filename"
     (is (= (parse-article-filename "2014-04-02-first-post.md")
            {:code "2014-04-02-first-post",
             :title "First Post",
             :month-name "April",
             :day "02", :month "04", :year "2014"})))
+
   (testing "group-by-month"
-    (is (= (group-by-month test-articles)
+    (is (= (group-by-month test-article-names)
            {"2014-05" ["2014-05-01-next-post.md"],
-            "2014-04" ["2014-04-02-first-post.md"]}))))
+            "2014-04" ["2014-04-15-april-post.md"
+                       "2014-04-02-first-post.md"]})))
+
+  (testing "sidebar-recent-article-data"
+    (is (= (sidebar-recent-article-data test-article-names)
+           [{:code "2014-05-01-next-post", :title "Next Post",
+             :month-name "May", :day "01", :month "05", :year "2014"}
+            {:code "2014-04-15-april-post", :title "April Post",
+             :month-name "April", :day "15", :month "04", :year "2014"}
+            {:code "2014-04-02-first-post", :title "First Post",
+             :month-name "April", :day "02", :month "04", :year "2014"}])))
+
+  (testing "sidebar-archive-data"
+    (is (= (sidebar-archive-data test-article-names)
+           [{:code "2014-05", :title "",
+             :month-name "May", :month "05", :year "2014"}
+            {:code "2014-04", :title "",
+             :month-name "April", :month "04", :year "2014"}]))))
---
+++ test/blog/article/article_handler_impl_test.clj
@@ -0,0 +1,92 @@
+(ns blog.article.article-handler-impl-test
+  (:use blog.article-plugin.plugin
+        blog.article.article-datastore)
+  (:require [clojure.test :refer :all]
+            [blog.article.article-handler-impl :refer :all]))
+
+; test plugins
+
+(defrecord AddIdPlugin [id]
+  Plugin
+    (process [this content] (str content id)))
+
+(def plugin-1 (map->AddIdPlugin {:id 1}))
+(def plugin-2 (map->AddIdPlugin {:id 2}))
+(def plugin-3 (map->AddIdPlugin {:id 3}))
+
+(def plugins-component {:first-plugin  plugin-1
+                        :second-plugin plugin-2
+                        :third-plugin  plugin-3
+                        :plugins [:first-plugin :second-plugin :third-plugin]})
+
+(deftest plugins
+  (testing "plugin-seq"
+    (is (= (plugin-seq plugins-component)
+           [plugin-1 plugin-2 plugin-3])))
+
+  (testing "apply-plugins"
+    (is (= (apply-plugins "abc" [plugin-1 plugin-2 plugin-3])
+           "abc123")))
+
+  (testing "apply-plugins-page"
+    (is (= (apply-plugins-page plugins-component
+                               {:items [{:body "abc"}
+                                        {:body "def"}
+                                        {:body "ghi"}]})
+           {:items [{:body "abc123"}
+                    {:body "def123"}
+                    {:body "ghi123"}]}))))
+
+
+(defrecord MockArticleDB []
+  ArticleDatastore
+    (article [this code]
+      (if (= "some-code" code)
+        {:items [{:body "abc"}]
+         :recent-articles "recent-articles"
+         :archive-months "archive-months"}))
+    (article-page [this page-num]
+      (if (= 0 page-num)
+        {:current-page page-num
+         :next-page 1
+         :items [{:body "abc"} {:body "def"}]
+         :recent-articles "recent-articles"
+         :archive-months "archive-months"}))
+    (article-month-page [this month page-num]
+      (if (= 0 page-num)
+        {:current-page page-num
+         :next-page 1
+         :items [{:body "def"} {:body "ghi"}]
+         :recent-articles "recent-articles"
+         :archive-months "archive-months"})))
+
+; test endpoints
+
+(def endpoints-component (merge plugins-component
+                                {:db (map->MockArticleDB {})}))
+
+(deftest endpoints
+  (testing "list-articles-page"
+    (is (= (list-articles-page endpoints-component 0)
+           {:data {:current-page 0,
+                   :next-page "/articles/page/1",
+                   :items '({:body "abc123"} {:body "def123"})
+                   :recent-articles "recent-articles"
+                   :archive-months "archive-months"},
+            :template :article-list})))
+
+  (testing "list-articles-month-page"
+    (is (= (list-articles-month-page endpoints-component "2014-04" 0)
+           {:data {:current-page 0,
+                   :next-page "/articles/month/2014-04/page/1",
+                   :items '({:body "def123"} {:body "ghi123"})
+                   :recent-articles "recent-articles"
+                   :archive-months "archive-months"},
+            :template :article-list})))
+
+  (testing "show-article"
+    (is (= (show-article endpoints-component "some-code")
+           {:data {:items '({:body "abc123"}),
+                   :recent-articles "recent-articles",
+                   :archive-months "archive-months"},
+            :template :article-list}))))
