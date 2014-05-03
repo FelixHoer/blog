5c01d93ccbcff886c42ad67f77f345fb818b04b1
FelixHoer <felix.hoerandner@gmx.at>
2014-05-03 19:56:08 +0200

    added tests for the comment component

---
+++ test/blog/comment/comment_handler_impl_test.clj
@@ -0,0 +1,94 @@
+(ns blog.comment.comment-handler-impl-test
+  (:require [clojure.test :refer :all]
+            [blog.comment.comment-handler-impl :refer :all]
+            [blog.comment.comment-sql-datastore :as cdb]
+            [blog.comment.comment-sql-datastore-impl :as cdbi]
+            [clojure.java.jdbc :as jdbc]))
+
+
+; test helper functions
+
+(deftest helpers
+  (testing "comment-name"
+    (are [req n] (= (comment-name req) n)
+         {}                                 ""
+         {:session {:username "name"}}      "name"
+         {:params {"comment-name" "name"}}  "name"
+         {:session {:username "name1"}
+          :params {"comment-name" "name2"}} "name1"))
+
+  (testing "comment-text"
+    (are [req t] (= (comment-text req) t)
+         {} ""
+         {:params {}} ""
+         {:params {"comment-text" "text"}} "text")))
+
+
+; test endpoints
+
+(defn traverse [m ks]
+  (reduce (fn [accu k] (k accu)) m ks))
+
+(def db-spec {:subprotocol "hsqldb"
+                :subname (str "mem:testdb"
+                              ";shutdown=true"
+                              ";sql.syntax_pgs=true")
+                :user "SA"
+                :password ""})
+
+(deftest endpoints
+  (jdbc/with-db-connection [con db-spec]
+    (let [com {:db (cdb/map->CommentSQLDatastore {:db con})}]
+
+      (testing "create-comment-table"
+        (is (= (cdbi/create-comment-table {:db con})
+               :ok)))
+
+      (testing "save-comment"
+        (are [code req resp] (= (save-comment com code req) resp)
+
+             "the-article"
+             {:params {"comment-name" ""
+                       "comment-text" "text"}
+              :server-name "localhost"
+              :server-port "8080"}
+             {:status 302,
+              :headers {"Location" "http://localhost:8080/articles/the-article"},
+              :body ""}
+
+             "the-article"
+             {:params {"comment-name" "name"
+                       "comment-text" "text"}
+              :server-name "localhost"
+              :server-port "8080"}
+             {:status 302,
+              :headers {"Location" "http://localhost:8080/articles/the-article#comments"},
+              :body ""}))
+
+      (testing "extend-with-comment-counts"
+        (are [resp1 resp2] (= (extend-with-comment-counts com resp1) resp2)
+
+             {:data {:items []}}
+             {:data {:items []}}
+
+             {:data {:items [{:code "the-article"}]}}
+             {:data {:items [{:code "the-article"
+                              :comment-count 1}]}}
+
+             {:data {:items [{:code "the-wrong-article"}]}}
+             {:data {:items [{:code "the-wrong-article"
+                              :comment-count 0}]}}))
+
+      (testing "extend-with-comments"
+        (are [resp1 checks] (let [resp2 (extend-with-comments com resp1)]
+                             (every? identity
+                                     (map (fn [[ks v]] (= v (traverse resp2 ks))) checks)))
+
+             {:data {:items [{:code "the-article"}]}}
+             [[[:data :items first :code] "the-article"]
+              [[:data :items first :comments first :name] "name"]
+              [[:data :items first :comments first :text] "text"]]
+
+             {:data {:items [{:code "the-wrong-article"}]}}
+             [[[:data :items first :code] "the-wrong-article"]
+              [[:data :items first :comments] []]])))))
---
+++ test/blog/comment/comment_sql_datastore_impl_test.clj
@@ -0,0 +1,78 @@
+(ns blog.comment.comment-sql-datastore-impl-test
+  (:require [clojure.test :refer :all]
+            [blog.comment.comment-sql-datastore-impl :refer :all]
+            [clojure.java.jdbc :as jdbc]))
+
+
+; test the validation function
+
+(deftest validation
+  (testing "validation-errors"
+    (are [c errs] (= (validation-errors c) errs)
+
+         nil             ["Name is blank."
+                         "Text is blank."]
+         {}             ["Name is blank."
+                         "Text is blank."]
+
+         {:name "name"} ["Text is blank."]
+         {:name "name"
+          :text ""}     ["Text is blank."]
+         {:name "name"
+          :text "    "} ["Text is blank."]
+
+         {:text "text"} ["Name is blank."]
+         {:name ""
+          :text "text"} ["Name is blank."]
+         {:name "    "
+          :text "text"} ["Name is blank."]
+
+         {:name (apply str (repeat 256 \a))
+          :text "text"} ["Name is too long."]
+
+         {:name "name"
+          :text "text"} nil)))
+
+
+; test functions that operate on the database
+
+(deftest database-operations
+  (let [db-spec {:subprotocol "hsqldb"
+                :subname (str "mem:testdb"
+                              ";shutdown=true"
+                              ";sql.syntax_pgs=true")
+                :user "SA"
+                :password ""}]
+    (jdbc/with-db-connection [con db-spec]
+
+      (testing "create-comment-table"
+        (is (= (create-comment-table {:db con})
+               :ok)))
+
+      (testing "insert-comment"
+        (is (= (insert-comment {:db con}
+                               {:name "some author"
+                                :text "some text"
+                                :time (java.util.Date.)}
+                       "the-article")
+               :ok)))
+
+      (testing "select-comments"
+        (let [[c] (select-comments {:db con} "the-article")]
+          (are [k v] (= (k c) v)
+               :name "some author"
+               :text "some text"))
+        (is (= (select-comments {:db con} "the-wrong-article")
+               [])))
+
+      (testing "select-comment-count"
+        (is (= (select-comment-count con "the-article")
+               1))
+        (is (= (select-comment-count con "the-wrong-article")
+               0)))
+
+      (testing "select-comment-counts"
+        (is (= (select-comment-counts {:db con} ["the-article"])
+               {"the-article" 1}))
+        (is (= (select-comment-counts {:db con} ["the-wrong-article"])
+               {"the-wrong-article" 0}))))))
